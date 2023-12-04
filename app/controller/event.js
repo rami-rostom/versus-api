ae213033136d4502f318b1c6c890ab424978fb41
Rami <rami.rostom@proton.me>
2023-12-04 21:49:25 +0100

    :sparkles: feat: add update and delete an event feature

--- app/controller/event.js
+++ app/controller/event.js
@@ -50,7 +50,73 @@ const controller = {
       console.log(error);
       res.status(500).json(error.toString());
     }
-  }
+  },
+
+  updateOneEvent: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const event = await Event.findByPk(id);
+      if (!event) {
+        return res.status(404).json({
+          'error': 'Event not found. Please verify the provided id.'
+        });
+      }
+
+      const {
+        title,
+        start_date,
+        end_date,
+        banner,
+        location,
+        status,
+        description,
+        rules,
+        contact,
+        result,
+        type_event_id,
+        game_id,
+        user_id
+      } = req.body;
+
+      if (title) { event.title = title; }
+      if (start_date) { event.start_date = start_date; }
+      if (end_date) { event.end_date = end_date; }
+      if (banner) { event.banner = banner; }
+      if (location) { event.location = location; }
+      if (status) { event.status = status; }
+      if (description) { event.description = description; }
+      if (rules) { event.rules = rules; }
+      if (contact) { event.contact = contact; }
+      if (result) { event.result = result; }
+      if (type_event_id) { event.type_event_id = type_event_id; }
+      if (game_id) { event.game_id = game_id; }
+      if (user_id) { event.user_id = user_id; }
+
+      await event.save();
+      res.json(event);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error.toString());
+    }
+  },
+
+  deleteOneEvent: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const event = await Event.findByPk(id);
+      if (!event) {
+        return res.status(404).json({
+          'error': 'Event not found. Please verify the provided id.'
+        });
+      }
+
+      await event.destroy();
+      res.json('Event deleted');
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error.toString());
+    }
+  },
 };
 
 module.exports = controller;
--- app/router/index.js
+++ app/router/index.js
@@ -6,6 +6,7 @@ const { eventController } = require('../controller');
 router.get('/event', eventController.getAllEvent);
 router.get('/event/:id', eventController.getOneEvent);
 router.post('/event', eventController.createOneEvent);
-// router.patch('event/:id', eventController.updateOneEvent);
+router.patch('/event/:id', eventController.updateOneEvent);
+router.delete('/event/:id', eventController.deleteOneEvent);
 
 module.exports = router;
