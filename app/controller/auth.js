d8e6a3d88ecc36f8bde33f24d131d0e1a6daf072
Rami <rami.rostom@proton.me>
2023-12-06 15:26:55 +0100

    :sparkles: feat: add signup feature

---
+++ app/controller/auth.js
@@ -0,0 +1,67 @@
+const bcrypt = require('bcrypt');
+const { User } = require('../models/index');
+
+const controller = {
+  handleSignUp: async (req, res) => {
+    const EMAIL_REGEX = /^[\w\.-]+@([\w-]+\.)+[\w-]{2,4}$/gm;
+
+    // Password require at least 8 characters, 1 capital letter and 1 number.
+    const PASSWORD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm;
+
+    try {
+      const { email, password, confirmation } = req.body;
+
+      if (!email || !password) {
+        return res.status(400).json({
+          'error': 'Missing parameter(s).'
+        });
+      }      
+
+      if (!EMAIL_REGEX.test(email)) {
+        return res.status(400).json({
+          'error': 'Email is not valid.'
+        });
+      }
+
+      if (!PASSWORD_REGEX.test(password)) {
+        return res.status(400).json({
+          'error': 'Password invalid (must be at least 8 characters, include 1 number and 1 capital letter).'
+        });
+      }
+
+      if (password !== confirmation) {
+        return res.status(400).json({
+          'error': 'Password and confirmation isn\'t matching.'
+        });
+      }
+
+      const emailCheck = await User.findOne({
+        where: { email }
+      });
+
+      if (emailCheck) {
+        return res.status(400).json({
+          'error': 'Email already used. Please try with a different email.'
+        });
+      }
+
+      const passwordHashed = await bcrypt.hash(password, 10);
+
+      await User.create({
+        email,
+        username: 'william',
+        password: passwordHashed,
+        confirmation: passwordHashed
+      });
+
+      res.status(200).json({
+        message: 'User created, you can now login'
+      });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error.toString());
+    }
+  }
+};
+
+module.exports = controller;
