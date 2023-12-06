94454a9ed2fa7f51bae2a0904f392f39f4c802b7
WilliamViranin <william.viranin@gmail.com>
2023-12-06 16:49:38 +0100

    :sparkles: feat: jwt = generation token

--- app/controller/auth.js
+++ app/controller/auth.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 const { v4: uuidv4 } = require('uuid');
+const jwt = require('../services/jwt');
 const { User } = require('../models/index');
 
 const controller = {
@@ -60,6 +61,44 @@ const controller = {
       res.status(200).json({
         message: 'User created, you can now login'
       });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error.toString());
+    }
+  },
+
+  handleSignIn: async (req, res) => {
+    try {
+      const { email, password } = req.body;
+
+      if (!email || !password) {
+        return res.status(400).json({
+          'error': 'Missing parameter(s).'
+        });
+      }
+
+      const userFound = await User.findOne({
+        where: { email }
+      });
+
+      if (!userFound) {
+        return res.status(400).json({
+          'error': 'This user doesn\'t exist.'
+        });
+      }
+
+      if (userFound) {
+        bcrypt.compare(password, userFound.password, verificationBcrypt());
+      }
+
+      const verificationBcrypt = (errBycrypt, resBycrypt) => {
+        if (resBycrypt) {
+          return res.status(200).json({
+            'userId': userFound.id,
+            'token': jwt.generateTokenForUser(userFound)
+          });
+        }
+      };
     } catch (error) {
       console.log(error);
       res.status(500).json(error.toString());
--- server.js
+++ server.js
@@ -2,8 +2,12 @@ require ('dotenv').config();
 
 const express = require('express');
 const router = require('./app/router');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
