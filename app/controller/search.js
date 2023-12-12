a9a41549b48be86fa955d5655535bc1389d42b85
Rami <rami.rostom@proton.me>
2023-12-12 14:56:43 +0100

    :sparkles: feat: add search feature

---
+++ app/controller/search.js
@@ -0,0 +1,115 @@
+const { Event } = require('../models/index');
+const { User } = require('../models/index');
+const { Team } = require('../models/index');
+const { Op } = require('sequelize');
+
+const controller = {
+  searchEvent: async (req, res) => {
+    try {
+      const eventTitle = req.query.q;
+
+      if (!eventTitle) {
+        return res.status(400).json({
+          'error': 'Missing event title in the query.'
+        });
+      }
+
+      const events = await Event.findAll({
+        where: { title: {
+          [Op.iLike]: `%${eventTitle}%`
+        }}
+      });
+
+      res.status(200).json(events);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error.toString());
+    }
+  },
+
+  searchUser: async (req, res) => {
+    try {
+      const username = req.query.q;
+
+      if (!username) {
+        return res.status(400).json({
+          'error': 'Missing username in the query.'
+        });
+      }
+
+      const users = await User.findAll({
+        where: { username: {
+          [Op.iLike]: `%${username}%`
+        }}
+      });
+
+      res.status(200).json(users);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error.toString());
+    }
+  },
+
+  searchTeam: async (req, res) => {
+    try {
+      const teamName = req.query.q;
+
+      if (!teamName) {
+        return res.status(400).json({
+          'error': 'Missing team name in the query.'
+        });
+      }
+
+      const teams = await Team.findAll({
+        where: { name: {
+          [Op.iLike]: `%${teamName}%`
+        }}
+      });
+
+      res.status(200).json(teams);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error.toString());
+    }
+  },
+
+  searchAll: async (req, res) => {
+    try {
+      const query = req.query.q;
+
+      if (!query) {
+        return res.status(400).json({
+          'error': 'Missing team name in the query.'
+        });
+      }
+
+      const events = await Event.findAll({
+        where: { title: {
+          [Op.iLike]: `%${query}%`
+        }}
+      });
+
+      const users = await User.findAll({
+        where: { username: {
+          [Op.iLike]: `%${query}%`
+        }}
+      });
+
+      const teams = await Team.findAll({
+        where: { name: {
+          [Op.iLike]: `%${query}%`
+        }}
+      });
+
+      // Merge users and teams array in event's array
+      const resultQuery = events.concat(users, teams);
+
+      res.status(200).json(resultQuery);
+    } catch (error) {
+      console.log(error);
+      res.status(500).json(error.toString());
+    }
+  }
+};
+
+module.exports = controller;
