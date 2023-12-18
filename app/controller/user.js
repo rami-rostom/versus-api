4f5f4b1d1392ea0ed0826c49d00e8d9e58a3419c
WilliamViranin <william.viranin@gmail.com>
2023-12-18 12:17:54 +0100

    :recycle: refactor: delete the try&catch blocks from user controller

--- app/controller/user.js
+++ app/controller/user.js
@@ -2,186 +2,158 @@ const { User } = require('../models/index');
 
 const controller = {
   getAllUsers: async (req, res) => {
-    try {
-      const users = await User.findAll();
+   
+    const users = await User.findAll();
 
-      res.status(200).json(users);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json(error.toString());
-    }
+    res.status(200).json(users);
   },
 
   getOneUser: async (req, res) => {
-    try {
-      const { idOrSlug } = req.params;
-      const isId = !isNaN(idOrSlug);
+    
+    const { idOrSlug } = req.params;
+    const isId = !isNaN(idOrSlug);
 
-      let user;
+    let user;
 
-      if (isId) {
-        user = await User.findByPk(idOrSlug, {
-          include: [
-            'role',
-            'socials',
-            'events',
-            'organize',
-            'teams',
-            'platforms',
-            'games',
-            'followers',
-            'like_teams',
-            'likeEvents'
-          ]
-        });
-      } else {
-        user = await User.findOne({
-          where: { username_slug: idOrSlug },
-          include: [
-            'role',
-            'socials',
-            'events',
-            'organize',
-            'teams',
-            'platforms',
-            'games',
-            'followers',
-            'like_teams',
-            'likeEvents'
-          ]
-        });
-      }
+    if (isId) {
+      user = await User.findByPk(idOrSlug, {
+        include: [
+          'role',
+          'socials',
+          'events',
+          'organize',
+          'teams',
+          'platforms',
+          'games',
+          'followers',
+          'like_teams',
+          'likeEvents'
+        ]
+      });
+    } else {
+      user = await User.findOne({
+        where: { username_slug: idOrSlug },
+        include: [
+          'role',
+          'socials',
+          'events',
+          'organize',
+          'teams',
+          'platforms',
+          'games',
+          'followers',
+          'like_teams',
+          'likeEvents'
+        ]
+      });
+    }
 
-      if (user) {
-        res.status(200).json(user);
-      } else {
-        res.status(404).json({
-          'error': 'User not found. Please verify the provided id.'
-        });
-      }
-    } catch (error) {
-      console.log(error);
-      res.status(500).json(error.toString());
+    if (user) {
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({
+        'error': 'User not found. Please verify the provided id.'
+      });
     }
   },
 
   updateOneUser: async (req, res) => {
-    try {
-      const { id } = req.params;
-      const user = await User.findByPk(id);
+   
+    const { id } = req.params;
+    const user = await User.findByPk(id);
 
-      if (!user) {
-        return res.status(404).json({
-          'error': 'User not found. Please verify the provided id.'
-        });
-      }
-
-      const { username, avatar } = req.body;
-
-      if (username) { user.username = username; }
-      if (avatar) { user.avatar = avatar; }
-
-      await user.save();
-      res.json(user);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json(error.toString());
+    if (!user) {
+      return res.status(404).json({
+        'error': 'User not found. Please verify the provided id.'
+      });
     }
+
+    const { username, avatar } = req.body;
+
+    if (username) { user.username = username; }
+    if (avatar) { user.avatar = avatar; }
+
+    await user.save();
+    res.json(user);
   },
 
   getUserEvents: async (req, res) => {
-    try {
-      const { id } = req.params;
-      const user = await User.findByPk(id);
+    
+    const { id } = req.params;
+    const user = await User.findByPk(id);
 
-      if (!user) {
-        return res.status(404).json({
-          'error': 'User not found. Please verify the provided id.'
-        });
-      }
-
-      const userEvents = await user.getEvents();
-
-      res.status(200).json(userEvents);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json(error.toString());
+    if (!user) {
+      return res.status(404).json({
+        'error': 'User not found. Please verify the provided id.'
+      });
     }
+
+    const userEvents = await user.getEvents();
+
+    res.status(200).json(userEvents);
   },
 
   getUserTeams: async (req, res) => {
-    try {
-      const { id } = req.params;
-      const user = await User.findByPk(id);
+   
+    const { id } = req.params;
+    const user = await User.findByPk(id);
 
-      if (!user) {
-        return res.status(404).json({
-          'error': 'User not found. Please verify the provided id.'
-        });
-      }
-
-      const userTeams = await user.getTeams();
-
-      res.status(200).json(userTeams);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json(error.toString());
+    if (!user) {
+      return res.status(404).json({
+        'error': 'User not found. Please verify the provided id.'
+      });
     }
+
+    const userTeams = await user.getTeams();
+
+    res.status(200).json(userTeams);
   },
 
   followUser: async (req, res) => {
-    try {
-      // User who wants to follow
-      const { user_id } = req.body;
+    
+    // User who wants to follow
+    const { user_id } = req.body;
 
-      // User to follow
-      const { id } = req.params;
+    // User to follow
+    const { id } = req.params;
 
-      const userFollower = await User.findByPk(user_id);
-      const userToFollow = await User.findByPk(id);
+    const userFollower = await User.findByPk(user_id);
+    const userToFollow = await User.findByPk(id);
 
-      if (!userToFollow) {
-        return res.status(404).json({
-          'error': 'User not found. Please verify the provided id.'
-        });
-      }
-
-      await userToFollow.addFollowing(userFollower, {
-        through: { user_liked_id: user_id }
+    if (!userToFollow) {
+      return res.status(404).json({
+        'error': 'User not found. Please verify the provided id.'
       });
-      res.status(200).json({ message: 'User followed successfully.' });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json(error.toString());
     }
+
+    await userToFollow.addFollowing(userFollower, {
+      through: { user_liked_id: user_id }
+    });
+    res.status(200).json({ message: 'User followed successfully.' });
   },
 
   unfollowUser: async (req, res) => {
-    try {
-      // User who wants to unfollow
-      const { user_id } = req.body;
+  
+    // User who wants to unfollow
+    const { user_id } = req.body;
 
-      // User to unfollow
-      const { id } = req.params;
+    // User to unfollow
+    const { id } = req.params;
 
-      const userFollower = await User.findByPk(user_id);
-      const userToUnfollow = await User.findByPk(id);
+    const userFollower = await User.findByPk(user_id);
+    const userToUnfollow = await User.findByPk(id);
 
-      if (!userToUnfollow) {
-        return res.status(404).json({
-          'error': 'User not found. Please verify the provided id.'
-        });
-      }
-
-      await userToUnfollow.removeFollowing(userFollower, {
-        through: { user_liked_id: user_id }
+    if (!userToUnfollow) {
+      return res.status(404).json({
+        'error': 'User not found. Please verify the provided id.'
       });
-      res.status(200).json({ message: 'User unfollowed successfully.' });
-    } catch (error) {
-      console.log(error);
-      res.status(500).json(error.toString());
     }
-  }
+
+    await userToUnfollow.removeFollowing(userFollower, {
+      through: { user_liked_id: user_id }
+    });
+    res.status(200).json({ message: 'User unfollowed successfully.' });
+  },
 };
 
 module.exports = controller;
