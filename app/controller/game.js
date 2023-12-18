25d4e515937d18e6795a8807111b2c851aa9aeab
WilliamViranin <william.viranin@gmail.com>
2023-12-18 12:29:52 +0100

    :recycle: refactor: delete the try&catch block from game controller

--- app/controller/game.js
+++ app/controller/game.js
@@ -2,15 +2,11 @@ const { Game } = require('../models/index');
 
 const controller = {
   getAllGames: async (_, res) => {
-    try {
-      const games = await Game.findAll();
+   
+    const games = await Game.findAll();
 
-      res.status(200).json(games);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json(error.toString());
-    }
-  }
+    res.status(200).json(games);
+  },
 };
 
 module.exports =  controller;
