de576881fa05d07280047bbaa16f30c0aa3d96f8
Rami <rami.rostom@proton.me>
2023-12-20 19:54:24 +0100

    :sparkles: feat: modify update user prefered games and platforms

--- app/controller/preferences.js
+++ app/controller/preferences.js
@@ -16,17 +16,10 @@ const controller = {
     // Use Sequelize method to get all favorites platforms of the user
     const userPlatforms = await user.getPlatforms();
 
-    for (const platform of platform_id) {
-      const isPrefered = userPlatforms.find((userPlatform) => userPlatform.id === platform);
-
-      if (!isPrefered) {
-        // Use Sequelize method to add a new entry in "user_like_platform" table
-        await user.addPlatforms(platform);
-      } else {
-        // Use Sequelize method to remove an entry from "user_like_platform" table
-        await user.removePlatforms(platform);
-      }
-    }
+    // Remove all platforms from favorites
+    await user.removePlatforms(userPlatforms);
+    // Add all new platforms to favorite
+    await user.addPlatforms(platform_id);
 
     res
       .status(200)
@@ -48,17 +41,10 @@ const controller = {
     // Use Sequelize method to get all favorites games of the user
     const userGames = await user.getGames();
 
-    for (const game of game_id) {
-      const isPrefered = userGames.find((userGame) => userGame.id === game);
-
-      if (!isPrefered) {
-        // Use Sequelize method to add a new entry in "user_like_game" table
-        await user.addGames(game);
-      } else {
-        // Use Sequelize method to remove an entry from "user_like_game" table
-        await user.removeGames(game);
-      }
-    }
+    // Remove all games from favorites
+    await user.removeGames(userGames);
+    // Add all new games to favorite
+    await user.addGames(game_id);
 
     res
       .status(200)
@@ -97,7 +83,73 @@ const controller = {
         .status(200)
         .json({ 'message': 'Event is now unfollow.' });
     }
-  }
+  },
+
+  // updateUserPreferedPlatform: async (req, res) => {
+  //   TOGGLE
+  //   const { id } = req.params;
+  //   const { platform_id } = req.body;
+
+  //   const user = await User.findByPk(id);
+
+  //   if (!user) {
+  //     return res
+  //       .status(404)
+  //       .json({ 'error': 'User not found. Please verify the provided id.' });
+  //   }
+
+  //   // Use Sequelize method to get all favorites platforms of the user
+  //   const userPlatforms = await user.getPlatforms();
+
+  //   for (const platform of platform_id) {
+  //     const isPrefered = userPlatforms.find((userPlatform) => userPlatform.id === platform);
+
+  //     if (!isPrefered) {
+  //       // Use Sequelize method to add a new entry in "user_like_platform" table
+  //       await user.addPlatforms(platform);
+  //     } else {
+  //       // Use Sequelize method to remove an entry from "user_like_platform" table
+  //       await user.removePlatforms(platform);
+  //     }
+  //   }
+
+  //   res
+  //     .status(200)
+  //     .json({ 'message': 'Platform preferences updated.' });
+  // },
+
+  // updateUserPreferedGame: async (req, res) => {
+  //   TOGGLE
+  //   const { id } = req.params;
+  //   const { game_id } = req.body;
+
+  //   const user = await User.findByPk(id);
+
+  //   if (!user) {
+  //     return res
+  //       .status(404)
+  //       .json({ 'error': 'User not found. Please verify the provided id.' });
+  //   }
+
+  //   // Use Sequelize method to get all favorites games of the user
+  //   const userGames = await user.getGames();
+
+  //   for (const game of game_id) {
+  //     const isPrefered = userGames.find((userGame) => userGame.id === game);
+
+  //     if (!isPrefered) {
+  //       // Use Sequelize method to add a new entry in "user_like_game" table
+  //       await user.addGames(game);
+  //     } else {
+  //       // Use Sequelize method to remove an entry from "user_like_game" table
+  //       await user.removeGames(game);
+  //     }
+  //   }
+
+  //   res
+  //     .status(200)
+  //     .json({ 'message': 'Game preferences updated.' });
+  // },
 };
 
 module.exports = controller;
